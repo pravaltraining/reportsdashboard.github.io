70c720bbdf9c55df4d509329ef7d4b0718568279
govardhan <govardhan.u@pravaltech.com>
2025-04-10 15:22:41 +0530

    added viteStaticcpoy

--- vite.config.ts
+++ vite.config.ts
@@ -97,12 +97,23 @@
 //     transformMixedEsModules: true
 //   }
 // })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: '/reportsdashboard.github.io/',  // for GitHub Pages
-  plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/Assets/icons/*',
+                    dest: 'assets' // copies into dist/assets/
+                }
+            ]
+        })
+    ],
   build: {
     rollupOptions: {
       external: [],
